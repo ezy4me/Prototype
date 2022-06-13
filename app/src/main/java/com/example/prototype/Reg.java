6cb6b2af114d735b4542416b798e2ab40b4a4230
SvenAventador <asumilkin67@gmail.com>
2022-06-13 15:08:02 +0300

    FirstCommitForMe

--- app/src/main/java/com/example/prototype/Reg.java
+++ app/src/main/java/com/example/prototype/Reg.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class Reg extends AppCompatActivity {
 
     public final int a = 0;
+public int b = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
